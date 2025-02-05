2db282c2d3dad2660cd4bce76e29b87fcf7b9a2d
RRenann <renancarlosmoreira71@gmail.com>
2025-02-05 18:59:33 -0300

    ajustes finais

--- src/MainContent/MainContent.js
+++ src/MainContent/MainContent.js
@@ -1,5 +1,21 @@
 import React from "react";
 import './MainContent.css';
+import logocard1 from '../../src/playlist/1.jpeg';
+import logocard2 from '../../src/playlist/2.png';
+import logocard3 from '../../src/playlist/3.jpeg';
+import logocard4 from '../../src/playlist/4.jpeg';
+import logocard5 from '../../src/playlist/5.jpeg';
+import logocard6 from '../../src/playlist/6.jpeg';
+import logocard7 from '../../src/playlist/7.jpeg';
+import logocard8 from '../../src/playlist/8.jpeg';
+import logocard9 from '../../src/playlist/9.jpeg';
+import logocard10 from '../../src/playlist/10.jpeg';
+import logocard11 from '../../src/playlist/11.jpeg';
+import logocard12 from '../../src/playlist/12.jpeg';
+import logocard13 from '../../src/playlist/13.jpeg';
+import logocard14 from '../../src/playlist/14.jpeg';
+import logocard15 from '../../src/playlist/15.jpeg';
+
 
 const MainContent = () => {
     return (
@@ -14,122 +30,122 @@ const MainContent = () => {
                     <div class="offer__scroll-container">
                         <div class="offer__list">
                             <section class="offer__list-item">
-                                {/* CARD */} 
+                                {/* CARD 1*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card1">
-                                        <img src="./src/assets/playlist/1.jpeg" alt="" />
+                                        <img src={logocard1} alt="" />
                                         <span>Boas festas</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 2*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card2">
-                                        <img src="./src/assets/playlist/2.png" alt="" />
+                                        <img src={logocard2} alt="" />
                                         <span>Feitos para você</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 3*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card3">
-                                        <img src="./src/assets/playlist/3.jpeg" alt="" />
+                                        <img src={logocard3} alt="" />
                                         <span>Lançamentos</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 4*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card4">
-                                        <img src="./src/assets/playlist/4.jpeg" alt="" />
+                                        <img src={logocard4} alt="" />
                                         <span>Creators</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 5*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card5">
-                                        <img src="./src/assets/playlist/5.jpeg" alt="" />
+                                        <img src={logocard5} alt="" />
                                         <span>Para treinar</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 6*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card6">
-                                        <img src="./src/assets/playlist/6.jpeg" alt="" />
+                                        <img src={logocard6} alt="" />
                                         <span>Podcasts</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 7*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card7">
-                                        <img src="./src/assets/playlist/7.jpeg" alt="" />
+                                        <img src={logocard7} alt="" />
                                         <span>Sertanejo</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 8*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card8">
-                                        <img src="./src/assets/playlist/8.jpeg" alt="" />
+                                        <img src={logocard8} alt="" />
                                         <span>Samba e pagode</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */}
+                                {/* CARD 9*/}
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card9">
-                                        <img src="./src/assets/playlist/9.jpeg" alt="" />
+                                        <img src={logocard9} alt="" />
                                         <span>Funk</span>
                                     </div>
                                 </a>
 
-                               
+                                 {/* CARD 10*/}
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card10">
-                                        <img src="./src/assets/playlist/10.jpeg" alt="" />
+                                        <img src={logocard10} alt="" />
                                         <span>MPB</span>
                                     </div>
                                 </a>
 
-                                
+                                  {/* CARD 11*/}
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card11">
-                                        <img src="./src/assets/playlist/11.jpeg" alt="" />
+                                        <img src={logocard11} alt="" />
                                         <span>Rock</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 12*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card12">
-                                        <img src="./src/assets/playlist/12.jpeg" alt="" />
+                                        <img src={logocard12} alt="" />
                                         <span>Hip Hop</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 13*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card13">
-                                        <img src="./src/assets/playlist/13.jpeg" alt="" />
+                                        <img src={logocard13} alt="" />
                                         <span>Indie</span>
                                     </div>
                                 </a>
 
-                                {/* CARD */} 
+                                {/* CARD 14*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card14">
-                                        <img src="./src/assets/playlist/14.jpeg" alt="" />
+                                        <img src={logocard14} alt="" />
                                         <span>Relax</span>
                                     </div>
                                 </a>
 
-                                 {/* CARD */} 
+                                 {/* CARD 15*/} 
                                 <a href="http://localhost:3000" class="cards">
                                     <div class="cards card15">
-                                        <img src="./src/assets/playlist/15.jpeg" alt="" />
+                                        <img src={logocard15} alt="" />
                                         <span>Música Latina</span>
                                     </div>
                                 </a>
